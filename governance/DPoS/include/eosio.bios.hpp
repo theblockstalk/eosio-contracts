2b761b3dc8bceafa07ec6f3b86ea895d1f9b8090
Jack Tanner <jack@jackandtheblockstalk.com>
2020-03-15 22:49:26 +0100

    centralized

---
+++ governance/DPoS/include/eosio.bios.hpp
@@ -0,0 +1,187 @@
+#pragma once
+
+#include <eosio/action.hpp>
+#include <eosio/crypto.hpp>
+#include <eosio/eosio.hpp>
+#include <eosio/fixed_bytes.hpp>
+#include <eosio/privileged.hpp>
+#include <eosio/producer_schedule.hpp>
+
+namespace eosiobios {
+
+   using eosio::action_wrapper;
+   using eosio::check;
+   using eosio::checksum256;
+   using eosio::ignore;
+   using eosio::name;
+   using eosio::permission_level;
+   using eosio::public_key;
+
+   struct permission_level_weight {
+      permission_level  permission;
+      uint16_t          weight;
+   };
+
+   struct key_weight {
+      eosio::public_key  key;
+      uint16_t           weight;
+   };
+
+   struct wait_weight {
+      uint32_t           wait_sec;
+      uint16_t           weight;
+   };
+
+   struct authority {
+      uint32_t                              threshold = 0;
+      std::vector<key_weight>               keys;
+      std::vector<permission_level_weight>  accounts;
+      std::vector<wait_weight>              waits;
+   };
+
+   struct block_header {
+      uint32_t                                  timestamp;
+      name                                      producer;
+      uint16_t                                  confirmed = 0;
+      checksum256                               previous;
+      checksum256                               transaction_mroot;
+      checksum256                               action_mroot;
+      uint32_t                                  schedule_version = 0;
+      std::optional<eosio::producer_schedule>   new_producers;
+   };
+
+   class [[eosio::contract("eosio.bios")]] bios : public eosio::contract {
+      public:
+         using contract::contract;
+
+         ACTION onblock( ignore<block_header> header );
+         
+         ACTION newaccount( name             creator,
+                          name             name,
+                          ignore<authority> owner,
+                          ignore<authority> active){}
+                          
+         ACTION updateauth(  ignore<name>  account,
+                           ignore<name>  permission,
+                           ignore<name>  parent,
+                           ignore<authority> auth ) {}
+
+         ACTION deleteauth( ignore<name>  account,
+                          ignore<name>  permission ) {}
+
+         ACTION linkauth(  ignore<name>    account,
+                         ignore<name>    code,
+                         ignore<name>    type,
+                         ignore<name>    requirement  ) {}
+
+         ACTION unlinkauth( ignore<name>  account,
+                          ignore<name>  code,
+                          ignore<name>  type ) {}
+
+         ACTION canceldelay( ignore<permission_level> canceling_auth, ignore<checksum256> trx_id ) {}
+
+         ACTION setcode( name account, uint8_t vmtype, uint8_t vmversion, const std::vector<char>& code ) {}
+
+         ACTION setabi( name account, const std::vector<char>& abi );
+
+         ACTION onerror( ignore<uint128_t> sender_id, ignore<std::vector<char>> sent_trx );
+
+         ACTION setpriv( name account, uint8_t is_priv );
+         
+         ACTION setalimits( name account, int64_t ram_bytes, int64_t net_weight, int64_t cpu_weight );
+
+         ACTION setprods( const std::vector<eosio::producer_authority>& schedule );
+
+         ACTION setparams( const eosio::blockchain_parameters& params );
+
+         ACTION reqauth( name from );
+
+         ACTION activate( const eosio::checksum256& feature_digest );
+
+         ACTION reqactivated( const eosio::checksum256& feature_digest );
+
+         TABLE abi_hash {
+            name              owner;
+            checksum256       hash;
+            uint64_t primary_key()const { return owner.value; }
+
+            EOSLIB_SERIALIZE( abi_hash, (owner)(hash) )
+         };
+
+         typedef eosio::multi_index< "abihash"_n, abi_hash > abi_hash_table;
+
+         struct [[eosio::table, eosio::contract("eosio.system")]] producer_info {
+            name                                                     owner;
+            double                                                   total_votes = 0;
+            eosio::public_key                                        producer_key;
+            bool                                                     is_active = true;
+            eosio::binary_extension<eosio::block_signing_authority>  producer_authority;
+
+            uint64_t primary_key()const { return owner.value;                             }
+            double   by_votes()const    { return is_active ? -total_votes : total_votes;  }
+            bool     active()const      { return is_active;                               }
+            void     deactivate()       { producer_key = public_key(); producer_authority.reset(); is_active = false; }
+
+            eosio::block_signing_authority get_producer_authority()const {
+               if( producer_authority.has_value() ) {
+                  bool zero_threshold = std::visit( [](auto&& auth ) -> bool {
+                     return (auth.threshold == 0);
+                  }, *producer_authority );
+                  if( !zero_threshold ) return *producer_authority;
+               }
+               return convert_to_block_signing_authority( producer_key );
+            }
+
+            template<typename DataStream>
+            friend DataStream& operator << ( DataStream& ds, const producer_info& t ) {
+               ds << t.owner
+                  << t.total_votes
+                  << t.producer_key
+                  << t.is_active
+                  << t.url
+                  << t.unpaid_blocks
+                  << t.last_claim_time
+                  << t.location;
+
+               if( !t.producer_authority.has_value() ) return ds;
+
+               return ds << t.producer_authority;
+            }
+
+            template<typename DataStream>
+            friend DataStream& operator >> ( DataStream& ds, producer_info& t ) {
+               return ds >> t.owner
+                        >> t.total_votes
+                        >> t.producer_key
+                        >> t.is_active
+                        >> t.url
+                        >> t.unpaid_blocks
+                        >> t.last_claim_time
+                        >> t.location
+                        >> t.producer_authority;
+            }
+         };
+         typedef eosio::multi_index< "producers"_n, producer_info,
+                               indexed_by<"prototalvote"_n, const_mem_fun<producer_info, double, &producer_info::by_votes>  >
+                             > producers_table;
+
+         using newaccount_action = action_wrapper<"newaccount"_n, &bios::newaccount>;
+         using updateauth_action = action_wrapper<"updateauth"_n, &bios::updateauth>;
+         using deleteauth_action = action_wrapper<"deleteauth"_n, &bios::deleteauth>;
+         using linkauth_action = action_wrapper<"linkauth"_n, &bios::linkauth>;
+         using unlinkauth_action = action_wrapper<"unlinkauth"_n, &bios::unlinkauth>;
+         using canceldelay_action = action_wrapper<"canceldelay"_n, &bios::canceldelay>;
+         using setcode_action = action_wrapper<"setcode"_n, &bios::setcode>;
+         using setabi_action = action_wrapper<"setabi"_n, &bios::setabi>;
+         using setpriv_action = action_wrapper<"setpriv"_n, &bios::setpriv>;
+         using setalimits_action = action_wrapper<"setalimits"_n, &bios::setalimits>;
+         using setprods_action = action_wrapper<"setprods"_n, &bios::setprods>;
+         using setparams_action = action_wrapper<"setparams"_n, &bios::setparams>;
+         using reqauth_action = action_wrapper<"reqauth"_n, &bios::reqauth>;
+         using activate_action = action_wrapper<"activate"_n, &bios::activate>;
+         using reqactivated_action = action_wrapper<"reqactivated"_n, &bios::reqactivated>;
+
+         private:
+         void bios::update_elected_producers( const block_timestamp& block_time );
+   };
+}
---
+++ governance/DPoS/src/eosio.bios.cpp
@@ -0,0 +1,106 @@
+#include <eosio.bios.hpp>
+
+namespace eosiobios {
+
+void bios::onblock( ignore<block_header> ) {
+   require_auth(get_self());
+
+   block_timestamp timestamp;
+   name producer;
+   _ds >> timestamp >> producer;
+
+   if( timestamp.slot - _gstate.last_producer_schedule_update.slot > 120 ) {
+      update_elected_producers( timestamp );
+   }
+}
+
+void system_contract::update_elected_producers( const block_timestamp& block_time ) {
+      auto idx = _producers.get_index<"prototalvote"_n>();
+
+      using value_type = std::pair<eosio::producer_authority, uint16_t>;
+      std::vector< value_type > top_producers;
+      top_producers.reserve(21);
+
+      for( auto it = idx.cbegin(); it != idx.cend() && top_producers.size() < 21 && 0 < it->total_votes && it->active(); ++it ) {
+         top_producers.emplace_back(
+            eosio::producer_authority{
+               .producer_name = it->owner,
+               .authority     = it->get_producer_authority()
+            },
+            it->location
+         );
+      }
+
+      if( top_producers.size() == 0 || top_producers.size() < _gstate.last_producer_schedule_size ) {
+         return;
+      }
+
+      std::sort( top_producers.begin(), top_producers.end(), []( const value_type& lhs, const value_type& rhs ) {
+         return lhs.first.producer_name < rhs.first.producer_name; // sort by producer name
+         // return lhs.second < rhs.second; // sort by location
+      } );
+
+      std::vector<eosio::producer_authority> producers;
+
+      producers.reserve(top_producers.size());
+      for( auto& item : top_producers )
+         producers.push_back( std::move(item.first) );
+
+      if( set_proposed_producers( producers ) >= 0 ) {
+         _gstate.last_producer_schedule_size = static_cast<decltype(_gstate.last_producer_schedule_size)>( top_producers.size() );
+      }
+   }
+
+void bios::setabi( name account, const std::vector<char>& abi ) {
+   abi_hash_table table(get_self(), get_self().value);
+   auto itr = table.find( account.value );
+   if( itr == table.end() ) {
+      table.emplace( account, [&]( auto& row ) {
+         row.owner = account;
+         row.hash  = eosio::sha256(const_cast<char*>(abi.data()), abi.size());
+      });
+   } else {
+      table.modify( itr, eosio::same_payer, [&]( auto& row ) {
+         row.hash = eosio::sha256(const_cast<char*>(abi.data()), abi.size());
+      });
+   }
+}
+
+void bios::onerror( ignore<uint128_t>, ignore<std::vector<char>> ) {
+   check( false, "the onerror action cannot be called directly" );
+}
+
+void bios::setpriv( name account, uint8_t is_priv ) {
+   require_auth( get_self() );
+   set_privileged( account, is_priv );
+}
+
+void bios::setalimits( name account, int64_t ram_bytes, int64_t net_weight, int64_t cpu_weight ) {
+   require_auth( get_self() );
+   set_resource_limits( account, ram_bytes, net_weight, cpu_weight );
+}
+
+void bios::setprods( const std::vector<eosio::producer_authority>& schedule ) {
+   require_auth( get_self() );
+   set_proposed_producers( schedule );
+}
+
+void bios::setparams( const eosio::blockchain_parameters& params ) {
+   require_auth( get_self() );
+   set_blockchain_parameters( params );
+}
+
+void bios::reqauth( name from ) {
+   require_auth( from );
+}
+
+void bios::activate( const eosio::checksum256& feature_digest ) {
+   require_auth( get_self() );
+   preactivate_feature( feature_digest );
+}
+
+void bios::reqactivated( const eosio::checksum256& feature_digest ) {
+   check( is_feature_activated( feature_digest ), "protocol feature is not activated" );
+}
+
+}
